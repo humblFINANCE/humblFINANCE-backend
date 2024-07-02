4adbbb8ccdd91195ba58dd1dc3ab870b22142457
jenningsFantini <jenningsfantini+github@gmail.com>
2024-07-02 17:15:43 +0100

    ✨ feat(user-table): add basic user-table functionality

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 
-# from .src.api.v1.user_table import router as user_table_router
-# from .src.core.config import Config
+from .src.api.v1.user_table import router as user_table_router
 from .src.core.config import Config
 
+# Setup API Configuration
 config = Config()
-# app = FastAPI(title=config.PROJECT_NAME)
+app = FastAPI(title=config.PROJECT_NAME)
 
-# app.include_router(user_table_router)
+# Add API Routes
+app.include_router(user_table_router)
 
-app = FastAPI()
 
 @app.get("/")
 async def root():
